fdfdb86b2d5dc4c4c4fcdb7ff29004879ca7ac34
Lucas10 <63660708+Lucas104-alt@users.noreply.github.com>
2022-11-18 17:20:36 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# -lucas11-luc-framework
+## LucFramework
+it is a node framework from scratch.
+install the project
+```git
+git clone https://github.com/Lucas104-alt/LucFramework.git
+```
+```npm
+cd (name for the project)
+npm i lucframework
+```
+This package is the configuration from scratch to create a web page
+use ejs express morgan packages and you have to learn express
+tutorial for this: https://www.youtube.com/watch?v=OVESuyVoPkI
+github: https://github.com/Lucas104-alt/LucFramework
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import { dirname, join } from "path"
+import {fileURLToPath} from "url"
+
+
+const app = express()
+
+app.get("/", (req, res) => res.render("index", )) //LucFramework -- Index.EJS
+
+app.get("/Hello", (req, res) => res.render("Hello")) //LucFramework -- Hello.EJS
+
+app.get("/allgood", (req, res) => res.render("allgood", )) //LucFramework -- AllGood.EJS
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(join(__dirname, "views"))
+
+app.set("views", join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.use(express.static(join(__dirname, "public")))
+
+
+app.listen(3000)
+console.log("The Server is ready on port http://localhost:3000/")
